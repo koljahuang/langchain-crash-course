a5cd95765db3c75b4a6b027b682243a3e39e1b04
kolja huang <chao.a.huang@capgemini.com>
2024-09-27 15:06:22 +0800

    kolja: add query from Chroma which embedding_functions is based on Hugging face

--- 4_rag/1b_rag_hf_basics.py
+++ 4_rag/1b_rag_hf_basics.py
@@ -1,32 +1,21 @@
 import os
+import chromadb
 
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from chromadb.utils import embedding_functions
 
 # Define the persistent directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 persistent_directory = os.path.join(current_dir, "db", "chroma_db")
 
-# Define the embedding model
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+client = chromadb.PersistentClient(persistent_directory)  # data stored in 'db' folder
+em = embedding_functions.SentenceTransformerEmbeddingFunction()  # default is all-MiniLM-L6-v2 model
+collection = client.get_collection(name='article', embedding_function=em)  # type: ignore
 
-# Load the existing vector store with the embedding function
-db = Chroma(persist_directory=persistent_directory,
-            embedding_function=embeddings)
-
-# Define the user's question
-query = "Who is Odysseus' wife?"
-
-# Retrieve relevant documents based on the query
-retriever = db.as_retriever(
-    search_type="similarity_score_threshold",
-    search_kwargs={"k": 3, "score_threshold": 0.9},
+question = "Who is Odysseus' wife?"
+collection = client.get_collection(name='article', embedding_function=em)  # type: ignore
+results = collection.query(
+    query_texts=question,
+    n_results=1
 )
-relevant_docs = retriever.invoke(query)
-
-# Display the relevant results with metadata
-print("\n--- Relevant Documents ---")
-for i, doc in enumerate(relevant_docs, 1):
-    print(f"Document {i}:\n{doc.page_content}\n")
-    if doc.metadata:
-        print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
+print(results)
